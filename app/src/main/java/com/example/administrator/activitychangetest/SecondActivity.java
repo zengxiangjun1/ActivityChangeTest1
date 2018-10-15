7c586de6055b120ef6e5a150aa5cc319bd4ce324
zengxiangjun1 <1282892521@qq.com>
2018-10-15 16:35:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/activitychangetest/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.activitychangetest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity
+    extends AppCompatActivity
+
+{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
